344286cb1f30af86d81926410530644cdbddc64b
f.qiang <f.qiang@gmail.com>
2012-02-16 03:54:46 +0000

    remove .d file

--- server/server_side/Debug/src/Log.d
+++
@@ -1,5 +0,0 @@
-src/Log.d: ../src/Log.cpp ../src/Log.h ../src/GlobalVariables.h
-
-../src/Log.h:
-
-../src/GlobalVariables.h:
--- server/server_side/Debug/src/ProbabilityPolicy.d
+++
@@ -1,8 +0,0 @@
-src/ProbabilityPolicy.d: ../src/ProbabilityPolicy.cpp \
- ../src/ProbabilityPolicy.h ../src/Log.h ../src/GlobalVariables.h
-
-../src/ProbabilityPolicy.h:
-
-../src/Log.h:
-
-../src/GlobalVariables.h:
